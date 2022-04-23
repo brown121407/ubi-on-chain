b3fb26a340e8c457b7152eb5b917704d9542ed78
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2022-04-23 17:18:43 +0300

    contracts: Add deploy task.

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,8 +1,33 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+task("deploy", "Deploy UBI contract")
+  .addParam("backend", "The backend's wallet address")
+  .addParam("dailyIncome", "How much to distribute daily to subscribers")
+  .setAction(async (taskArgs) => {
+    const ubiFactory = await hre.ethers.getContractFactory("UBI");
+    const ubi = await ubiFactory.deploy(taskArgs.backend, hre.ethers.utils.parseEther(taskArgs.dailyIncome));
+
+    await ubi.deployed();
+
+    console.log("UBI deployed to:", ubi.address);
+  });
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4"
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {},
+    localhost1: {
+      url: "http://127.0.0.1:8545"
+    },
+    localhost2: {
+      url: "http://127.0.0.1:8546"
+    },
+    localhost3: {
+      url: "http://127.0.0.1:8547"
+    }
+  }
 };
